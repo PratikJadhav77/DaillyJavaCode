e5f1731a380394d799050352ebd85582546864de
PratikJadhav77 <pratikjadhav8170@gmail.com>
2025-07-31 12:21:54 +0530

    Add files via upload

---
+++ Default Constructor.java
@@ -0,0 +1,10 @@
+ class Car {
+    Car() {
+        System.out.println("Car object is created!");
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car(); // Constructor is called
+    }
+}
+
